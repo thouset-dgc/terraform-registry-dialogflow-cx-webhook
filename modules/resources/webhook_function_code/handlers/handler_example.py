389eec77753c9eb947ddd607d47ec2efd043dc49
Jules Rubin <58521821+julesrubin@users.noreply.github.com>
2025-01-15 19:41:35 +0100

    refactor(handler_example): update docstring and logging message for clarity

--- modules/resources/webhook_function_code/handlers/handler_example.py
+++ modules/resources/webhook_function_code/handlers/handler_example.py
@@ -7,9 +7,9 @@ logger = get_logger(__name__.split('.')[-1])
 
 @dataclass
 class HandlerExample(HandlerBase):
-    """Example handler for the 'example' tag. This should be used as a template for creating new handlers."""
+    """Example handler for the 'handler_example' tag. This should be used as a template for creating new handlers."""
     def __call__(self, request):
-        logger.info("Processing request with 'example' handler.")
+        logger.info("Processing request with 'handler_example' handler.")
         return {
             "message_text": ["Hello, World!"],
             "parameters": {"foo": "bar"},
