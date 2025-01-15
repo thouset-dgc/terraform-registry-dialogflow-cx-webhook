d4129dd035098b41a2a73fc824f78f005d93df80
Jules Rubin <58521821+julesrubin@users.noreply.github.com>
2025-01-15 18:34:26 +0100

    refactor(handler_example): add logging and improve request processing feedback

--- modules/resources/webhook_function_code/handlers/handler_example.py
+++ modules/resources/webhook_function_code/handlers/handler_example.py
@@ -1,11 +1,15 @@
 from dataclasses import dataclass
-from utils.base import HandlerBase
 
+from utils.base import HandlerBase
+from utils.logger import get_logger
+
+logger = get_logger(__name__.split('.')[-1])
 
 @dataclass
 class HandlerExample(HandlerBase):
     """Example handler for the 'example' tag. This should be used as a template for creating new handlers."""
     def __call__(self, request):
+        logger.info("Processing request with 'example' handler.")
         return {
             "message_text": ["Hello, World!"],
             "parameters": {"foo": "bar"},
